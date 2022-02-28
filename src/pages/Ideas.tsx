972317dee646b49444eb3b3d7e9b102d072e255b
ivan-ngchakming <ivan.ng.chak.ming@gmail.com>
2022-02-28 12:38:25 +0800

    feat: configured routes and page placeholders

---
+++ src/pages/Ideas.tsx
@@ -0,0 +1,7 @@
+import { Box } from "@mui/material";
+
+const Ideas = () => {
+  return <Box m={4}>Ideas page placeholder</Box>;
+};
+
+export default Ideas;
---
+++ src/pages/Settings.tsx
@@ -0,0 +1,7 @@
+import { Box } from "@mui/material";
+
+const Settings = () => {
+  return <Box m={4}>Settings page placeholder</Box>;
+};
+
+export default Settings;
