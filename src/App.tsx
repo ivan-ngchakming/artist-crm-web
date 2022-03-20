b945fe1cb0fa3c97d78b156639c5bf9f9a430e40
ivan-ngchakming <ivan.ng.chak.ming@gmail.com>
2022-03-20 18:30:52 +0800

    build: add react-cookies

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { HelmetProvider } from "react-helmet-async";
 import axios from "axios";
+import { CookiesProvider } from "react-cookie";
 import { NavBar, MetaTitle } from "./components";
 import {
   Analytics,
@@ -22,27 +23,32 @@ const queryClient = new QueryClient();
 const App = () => {
   return (
     <HelmetProvider>
-      <QueryClientProvider client={queryClient}>
-        <Router>
-          <ThemeProvider theme={theme}>
-            <CssBaseline />
-            <Box display="flex">
-              <MetaTitle />
-              <NavBar />
-              <Box width="100%" height="100vh" sx={{ overflowY: "auto" }}>
-                <Routes>
-                  <Route path="/" element={<Home />} />
-                  <Route path="/customers" element={<Customers />} />
-                  <Route path="/communications" element={<Communications />} />
-                  <Route path="/ideas" element={<Ideas />} />
-                  <Route path="/analytics" element={<Analytics />} />
-                  <Route path="/settings" element={<Settings />} />
-                </Routes>
+      <CookiesProvider>
+        <QueryClientProvider client={queryClient}>
+          <Router>
+            <ThemeProvider theme={theme}>
+              <CssBaseline />
+              <Box display="flex">
+                <MetaTitle />
+                <NavBar />
+                <Box width="100%" height="100vh" sx={{ overflowY: "auto" }}>
+                  <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/customers" element={<Customers />} />
+                    <Route
+                      path="/communications"
+                      element={<Communications />}
+                    />
+                    <Route path="/ideas" element={<Ideas />} />
+                    <Route path="/analytics" element={<Analytics />} />
+                    <Route path="/settings" element={<Settings />} />
+                  </Routes>
+                </Box>
               </Box>
-            </Box>
-          </ThemeProvider>
-        </Router>
-      </QueryClientProvider>
+            </ThemeProvider>
+          </Router>
+        </QueryClientProvider>
+      </CookiesProvider>
     </HelmetProvider>
   );
 };
